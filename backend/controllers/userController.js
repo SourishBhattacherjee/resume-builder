d254dd9bcdd28534e57289ded13797118ec9cadf
SourishBhattacherjee <sourishbhatt3302@gmail.com>
2025-07-10 20:39:14 +0530

    JWT signed. Verification left.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const registerUser = async (req,res) => {
   try {
@@ -26,4 +27,33 @@ const registerUser = async (req,res) => {
   }
 }
 
-module.exports = {registerUser}
+const loginUser = async (req,res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+    // Create JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email,fullName: user.fullName }, //payload
+      process.env.SECRET_KEY
+    );
+
+    res.status(200).json({
+      message: 'Login successful',
+      token,
+      user: { fullName: user.fullName, email: user.email }
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+}
+
+const getUser = async (req,res) =>{
+  
+}
+
+module.exports = {registerUser,loginUser,getUser}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userController');
+const { registerUser, loginUser,getUser } = require('../controllers/userController');
 const router = express.Router();
 
 
 router.post('/register',registerUser);
+router.post('/login',loginUser);
+router.get('/profile',getUser);
 
 module.exports = router;
