03d3a36751db1f3734a19d0cb6c868414d49757e
SourishBhattacherjee <sourishbhatt3302@gmail.com>
2025-07-11 22:58:13 +0530

    added basic register frontend. No api integration yet.

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Register = () => {
+  const [data,setData] = useState({
+    fullName:'',
+    email: '',
+    password:''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>Register</div>
+    <>
+    <form>
+      <label>Full Name:</label>
+      <input 
+      type = 'text'
+      placeholder='please enter you full name'/>
+      <label>Email:</label>
+      <input 
+      type = 'text'
+      placeholder='please enter you email'/>
+      <label>Password:</label>
+      <input 
+      type = 'text'
+      placeholder='please enter you password'/>
+      <button type='button' onClick={handleSubmit} className='bg-red-800 border-radius-10'>Sign Up</button>
+    </form>
+    </>
   )
 }
 
