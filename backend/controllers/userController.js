582a4edf510bf3e9186461bb7fae455a01b2191f
SourishBhattacherjee <sourishbhatt3302@gmail.com>
2025-07-20 18:27:08 +0530

    .:

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,7 +39,8 @@ const loginUser = async (req,res) => {
     if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
     const token = jwt.sign(
       { userId: user._id, email: user.email,fullName: user.fullName }, //payload
-      process.env.SECRET_KEY
+      process.env.SECRET_KEY,
+       { expiresIn: '1h' }
     );
 
     res.status(200).json({
